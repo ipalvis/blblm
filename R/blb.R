5f005da97b7400cfd9f88ea6c01ce241bd9aa0b2
Randy Lai <randy.cs.lai@gmail.com>
2020-03-10 23:07:40 -0700

    project hints

---
+++ R/blb.R
@@ -0,0 +1,104 @@
+#' @import purrr
+#' @import stats
+#' @importFrom magrittr %>%
+#' @details
+#' Linear Regression with Little Bag of Bootstraps
+"_PACKAGE"
+
+#' @export
+blblm <- function(formula, data, m = 10, B = 5000) {
+  data_list <- split_data(data, m)
+  estimates <- map(data_list, ~lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+  res <- list(estimates = estimates, formula = formula)
+  class(res) <- "blblm"
+  invisible(res)
+}
+
+
+#' split data into m parts of approximated equal sizes
+split_data <- function(data, m) {
+  idx <- sample.int(m, nrow(data), replace = TRUE)
+  data %>% split(idx)
+}
+
+
+#' compute the estimates
+lm_each_subsample <- function(formula, data, n, B) {
+  replicate(B, lm_each_boot(formula, data, n), simplify = FALSE)
+}
+
+
+#' compute the regression estimates for a blb dataset
+lm_each_boot <- function(formula, data, n) {
+  freqs <- rmultinom(1, n, rep(1, nrow(data)))
+  lm1(formula, data, freqs)
+}
+
+
+#' estimate the regression estimates based on given the number of repetitions
+lm1 <- function(formula, data, freqs) {
+  # drop the original closure of formula,
+  # otherwise the formula will pick a wront variable from the global scope.
+  environment(formula) <- environment()
+  fit <- lm(formula, data, weights = freqs)
+  list(coef = blbcoef(fit), sigma = blbsigma(fit, freqs))
+}
+
+
+#' compute the coefficients from fit
+blbcoef <- function(fit, freqs) {
+  # YOUR CODE to compute the coefficients
+}
+
+
+#' compute sigma from fit
+blbsigma <- function(fit, freqs) {
+  p <- fit$rank
+  y <- model.extract(fit$model, "response")
+  e <- fitted(fit) - y
+  # YOUR CODE to compute sigma
+}
+
+
+#' @export
+#' @method print blblm
+print.blblm <- function(x, ...) {
+  cat("blblm model")
+}
+
+
+#' @export
+#' @method sigma blblm
+sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
+  est <- object$estimates
+  # YOUR CODE to compute sigma and its c.i.
+}
+
+#' @export
+#' @method coef blblm
+coef.blblm <- function(object, ...) {
+  est <- object$estimates
+  # YOUR CODE to compute the coefficients.
+}
+
+
+#' @export
+#' @method confint blblm
+confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
+  if (is.null(parm)) {
+    parm <- attr(terms(fit$formula), "term.labels")
+  }
+  # YOUR CODE to compute the confidence intervals
+}
+
+#' @export
+#' @method predict blblm
+predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95,...) {
+  est <- object$estimates
+  X <- model.matrix(reformulate(attr(terms(object$formula), "term.labels")), new_data)
+  if (confidence) {
+    # YOUR CODE to compute the predictions and their confidence intervals
+  } else {
+    # YOUR CODE to compute the predictions
+  }
+}
